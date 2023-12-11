0419ce3412a6da807a04449eadf864e8e959afbe
Shivam Patel <shivampatel887@gmail.com>
2023-12-11 16:46:53 +0530

    adding extra things

--- login.py
+++ login.py
@@ -4,25 +4,41 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from dotenv import load_dotenv
 import os
+from selenium.webdriver.chrome.options import Options
 
 load_dotenv()
 
+headless = os.environ.get("HEADLESS")
 email = os.environ.get("LOGIN_EMAIL")
 password = os.environ.get("LOGIN_PASSWORD")
 
-driver = webdriver.Firefox()
+options = Options()
+
+if headless:
+    options.add_argument('--headless')
+    options.add_argument('--disable-gpu')
+
+driver = webdriver.Chrome(options=options)
+
 driver.get("https://www.oneplay.in/dashboard/login")
+print("Website opened...")
 
 email_input = driver.find_element(By.NAME, "name")
 password_input = driver.find_element(By.NAME, "password")
 
 email_input.send_keys(email)
+print("Email Entered...")
+
 password_input.send_keys(password)
+print("Password Entered...")
 
 login_btn = driver.find_element(By.XPATH, "//input[@type='submit']")
 login_btn.click()
-
 time.sleep(5)
 
+if (driver.current_url == "https://www.oneplay.in/dashboard/home"):
+    print(f"Login Successful")
+else:
+    print("Login Failed")
 
 driver.close()
